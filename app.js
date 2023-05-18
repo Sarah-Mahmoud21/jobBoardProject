9485731bc86668e794e725447fc61f6933389f56
Sarah <saramahmoud15420@gmail.com>
2023-05-18 17:12:50 +0300

    2ndpull

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const pool  = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
     user            : 'root',
-    password        : 'password',
+    password        : '',
     database        : 'job_board'
 })
 
