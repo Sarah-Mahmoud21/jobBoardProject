fc363eae3b8ab05c816050c23726008c6d76162a
RuaAwaysa123 <113847802+RuaAwaysa123@users.noreply.github.com>
2023-05-22 01:47:36 +0300

    update

--- applications.js
+++ applications.js
@@ -81,7 +81,109 @@ const applications = (pool) => {
     });
   });
 
-  // Add more routes as needed...
+   // Delete an application by ID
+   router.delete('/:application_id', (req, res) => {
+    const application_id = req.params.application_id;
+    const query = 'DELETE FROM applications WHERE application_id = ?';
+
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      connection.query(query, [application_id], (err, result) => {
+        connection.release();
+
+        if (err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+
+        if (result.affectedRows === 0) {
+          res.status(404).send(`Application with ID: ${application_id} not found.`);
+        } else {
+          res.send(`Application with ID: ${application_id} has been deleted.`);
+        }
+      });
+    });
+  });
+
+  // // Perform a filtered job search
+  // router.get('/jobs', (req, res) => {
+  //   const { title, location, salary } = req.query;
+
+  //   pool.getConnection((err, connection) => {
+  //     if (err) throw err;
+  //     console.log(`Connected as id ${connection.threadId}`);
+
+  //     const searchQuery = 'SELECT * FROM jobs WHERE title LIKE ? AND location LIKE ? AND salary >= ?';
+  //     const searchParams = [`%${title}%`, `%${location}%`, salary || 0];
+
+  //     connection.query(searchQuery, searchParams, (err, results) => {
+  //       connection.release();
+  //       if (!err) {
+  //         res.json(results);
+  //       } else {
+  //         console.log(err);
+  //         res.status(500).send('Internal Server Error');
+  //       }
+  //     });
+  //   });
+  // });
+// Perform a filtered job search and save the search
+router.get('/jobs', (req, res) => {
+  const { job_seeker_id, search_title, title, location, salary } = req.query;
+
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    const searchQuery = 'SELECT * FROM jobs WHERE title LIKE ? AND location LIKE ? AND salary >= ?';
+    const searchParams = [`%${title}%`, `%${location}%`, salary || 0];
+
+    connection.query(searchQuery, searchParams, (err, results) => {
+      if (!err) {
+        const savedSearch = { job_seeker_id, search_title, title, location, salary };
+        connection.query('INSERT INTO savedsearches SET ?', savedSearch, (err, result) => {
+          connection.release();
+          if (!err) {
+            res.json(results);
+          } else {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+          }
+        });
+      } else {
+        connection.release();
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  });
+});
+  // // Save a job search
+  // router.post('/save-search', (req, res) => {
+  //   const { job_seeker_id, search_title, title, location, salary } = req.body;
+  //   const savedSearch = { job_seeker_id, search_title, title, location, salary };
+
+  //   pool.getConnection((err, connection) => {
+  //     if (err) throw err;
+  //     console.log(`Connected as id ${connection.threadId}`);
+
+  //     connection.query('INSERT INTO savedsearches SET ?', savedSearch, (err, result) => {
+  //       connection.release();
+  //       if (!err) {
+  //         res.send(`Job search saved with the record ID: ${result.insertId}`);
+  //       } else {
+  //         console.log(err);
+  //         res.status(500).send('Internal Server Error');
+  //       }
+  //     });
+  //   });
+  // });
 
   return router;
 };
